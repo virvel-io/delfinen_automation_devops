fe8c27d119e62753f51e6afbd99c9e45bb2a33bf
Larissa Dovgoborets <larissa.dovgoborets@outlook.com>
2024-11-09 12:52:18 +0100

    change order of app contents, change labels on the graph

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,30 +21,39 @@ def layout():
     else:
         lat, lon = 59.3293, 18.0686
         
-
     st.markdown("## Temperature")
-    st.write(f"Showing temperature at latitude {lat} and longitude {lon}.")
     current_temp = WeatherData(lat=lat, lon=lon, api_key=os.getenv('api_key')).get_current_temp()
+    st.metric("Current temperature", f"{int(current_temp)} °C")
+    st.write(f"Showing temperature at latitude {lat} and longitude {lon}.")
 
-    st.metric("Current temperature", int(current_temp), "°C")
 
     st.markdown("## Hourly forecast for the next 24 hours")
     temp_next_24h = WeatherData(lat=lat, lon=lon, api_key=os.getenv('api_key')).get_temp_next_24h()
-    df = pd.DataFrame(temp_next_24h, columns=['Time', 'Temperature'])
-    st.write(df)
-    plt.plot(df['Time'], df['Temperature'])
-    fig, ax = plt.subplots()
-    ax.plot(df['Time'], df['Temperature'])
-    ax.xaxis.set_major_locator(mdates.HourLocator(interval=1))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H'))
-    plt.xlabel('Time')
-    plt.ylabel('Temperature (°C)')
-    plt.title('Temperature forecast for the next 24 hours')
-    st.pyplot(plt)
+    col1, col2 = st.columns([2,1])
+
+    with col2:
+        df = pd.DataFrame(temp_next_24h, columns=['Time', 'Temperature'])
+        st.write(df)
+
+    with col1:
+        fig, ax = plt.subplots()
+        ax.plot(df['Time'], df['Temperature'])
+        for i, value in enumerate(df['Temperature']):
+            plt.text(df['Time'][i], df['Temperature'][i], f"{round(value)}°", fontsize=8, ha='left', va='bottom')
+        ax.set_xlim(min(df['Time']), max(df['Time']))
+        ax.xaxis.set_tick_params(rotation=45, labelsize=9)
+        ax.yaxis.set_tick_params(labelsize=9)
+        ax.xaxis.set_major_locator(mdates.HourLocator(interval=1))
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%H'))
+        ax.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        plt.xlabel('Time')
+        plt.ylabel('Temperature (°C)')
+        plt.title('Temperature forecast for the next 24 hours')
+        st.pyplot(plt)
 
 
-
-    
 
 
 if __name__ == "__main__":
